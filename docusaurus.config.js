656fcc385181154156c4ec14170063ca5281355c
cee-diksha <cee.diksha@gmail.com>
2024-08-09 10:16:36 +0530

    header-footer modifications

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,68 +47,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
           src: 'img/logo.svg',
         },
         items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
         ],
       },
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `COPYRIGHT © 2024 Crimson Energy Experts Private Limited, All rights Reserved`,
       },
       prism: {
         theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
       },
     }),
    
--- src/pages/RangeZoomControl.jsx
+++ src/pages/RangeZoomControl.jsx
@@ -11,11 +11,11 @@ const RangeZoomControl = () => {
         <div className={styles.paddingleft}>
         The Zoom Bar offers intuitive controls for adjusting the view:
           <ul>
-            <li>Plus (+) Button: Click to zoom in and get a closer view of the map.</li>
-            <li>Minus (-) Button: Click to zoom out and see a broader area.</li>
-            <li>Home Button: Click to instantly reset the view to the IOR (Indian Ocean Region), providing a convenient way to return to the default map focus.</li>
+            <li><strong>Plus (+) Button:</strong> Click to zoom in and get a closer view of the map.</li>
+            <li><strong>Minus (-) Button:</strong> Click to zoom out and see a broader area.</li>
+            <li><strong>Home Button:</strong> Click to instantly reset the view to the IOR (Indian Ocean Region), providing a convenient way to return to the default map focus.</li>
           </ul>
-          <img src="../../static/img/gui/rangeZoom.png" alt="Image" style={{ width: 'auto', height: '350px' }} />
+          <img src="../../static/img/gui/rangeZoom.png" alt="Image" style={{ width: 'auto', height: '300px' }} />
         </div>
       </div>
     </div>
